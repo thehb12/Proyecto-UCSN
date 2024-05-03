57060d76ca73425984ec8a30b2702a8ef9972933
Miguel Jaimes <168801072+JaimesMs10@users.noreply.github.com>
2024-05-03 00:55:13 -0500

    actualizajaimes

--- src/config.py
+++ src/config.py
@@ -1,6 +1,6 @@
 # config.py
 class Config:
     MYSQL_HOST = 'localhost'
-    MYSQL_USER = 'clinica_san_nicolas'
-    MYSQL_PASSWORD = '1014'
-    MYSQL_DB = 'unidad_clinica_san_nicolas'
+    MYSQL_USER = 'root'
+    MYSQL_PASSWORD = ''
+    MYSQL_DB = 'prueba'
